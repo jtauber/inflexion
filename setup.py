725c7c780c582b2380bb0add8a964d9b3bdbc26d
James Tauber <jtauber@jtauber.com>
2016-04-23 03:53:37 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name="inflexion",
+    version="0.1",
+    description="generic code for morphological analysis (extracted from work on Ancient Greek)",
+    license="MIT",
+    url="http://github.com/jtauber/inflexion",
+    author="James Tauber",
+    author_email="jtauber@jtauber.com",
+    packages=["inflexion"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Topic :: Text Processing",
+        "Topic :: Text Processing :: Linguistic",
+    ],
+)
