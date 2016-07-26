ba6bb08cad639b1de6da37f97e2008ee030d6ce5
James Tauber <jtauber@jtauber.com>
2016-07-25 23:33:52 -0400

    bumped version to 0.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="inflexion",
-    version="0.5",
+    version="0.6",
     description="generic code for morphological analysis",
     license="MIT",
     url="http://github.com/jtauber/inflexion",
